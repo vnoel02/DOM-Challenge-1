50c873255dcc3ab91c0cc7a8ce9c54633148aa7d
victornoel <vnoel890@gmail.com>
2023-06-14 13:07:38 -0400

    added script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+// 1 - Select the section with an id of container without using querySelector.
+document.getElementById("container");
+
+// 2 - Select the section with an id of container using querySelector.
+
+document.querySelector("container");
+
+// 3 - Select all of the list items with a class of "second".
+
+document.querySelectorAll(".second")
+
+// 4 - Select a list item with a class of third, but only the list item inside of the ol tag.
+
+// 5 - Give the section with an id of container the text "Hello!".
+
+// 6 - Add the class main to the div with a class of footer.
+
+// 7 - Remove the class main on the div with a class of footer.
+
+// 8 - Create a new li element.
+
+// 9 - Give the li the text "four".
+
+// 10 - Append the li to the ul element.
+
+// 11 - Loop over all of the lis inside the ol tag and give them a background color of "green".
+
+// 12 - Remove the div with a class of footer.
+
